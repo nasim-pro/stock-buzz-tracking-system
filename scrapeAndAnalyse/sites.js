2d212b856e03bf79566ee2b4de3591dd084caf3d
Nasim Ahmad <khannasimahmad@gmail.com>
2025-08-30 13:09:45 +0530

    added ore sources

--- scrapeAndAnalyse/sites.js
+++ scrapeAndAnalyse/sites.js
@@ -3,32 +3,49 @@
  * Each site has: url, selectors for headline, link, date
  */
 export const SITES = [
+    // {
+    //     name: "moneycontrol",
+    //     url: "https://www.moneycontrol.com/news/business/stocks/",
+    // },
+    // {
+    //     name: "economictimes",
+    //     url: "https://economictimes.indiatimes.com/markets/stocks",
+    // },
+    // {
+    //     name: "livemint",
+    //     url: "https://www.livemint.com/market",
+    // },
+    // {
+    //     name: "businessstandard",
+    //     url: "https://www.business-standard.com/markets/news"
+    // },
+    // {
+    //     name: "financialexpress",
+    //     url: "https://www.financialexpress.com/market/",
+    // },
+    // {
+    //     name: "ndtvprofit",
+    //     url: "https://www.ndtv.com/business/market",
+    // },
+    // {
+    //     name: "zeebusiness",
+    //     url: "https://www.zeebiz.com/markets",
+    // },
+// newly added sites
     {
-        name: "moneycontrol",
-        url: "https://www.moneycontrol.com/news/business/stocks/",
+        name: "pulse",
+        url: "https://pulse.zerodha.com/",
     },
     {
-        name: "economictimes",
-        url: "https://economictimes.indiatimes.com/markets/stocks",
+        name: "finshots",
+        url: "https://finshots.in/",
     },
     {
-        name: "livemint",
-        url: "https://www.livemint.com/market",
+        name: "forbesindia",
+        url: "https://www.forbesindia.com/",
     },
     {
-        name: "businessstandard",
-        url: "https://www.business-standard.com/markets/news"
-    },
-    {
-        name: "financialexpress",
-        url: "https://www.financialexpress.com/market/",
-    },
-    {
-        name: "ndtvprofit",
-        url: "https://www.ndtv.com/business/market",
-    },
-    {
-        name: "zeebusiness",
-        url: "https://www.zeebiz.com/markets",
+        name: "financialchronicle",
+        url: "http://epaper.mydigitalfc.com/",
     }
 ];
