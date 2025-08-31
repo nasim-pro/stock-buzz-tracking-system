a9a57645897fb3b4d35e47fb5eb1e55f755d6e0a
Nasim Ahmad <khannasimahmad@gmail.com>
2025-08-31 13:03:46 +0530

    added least ranked route

--- routes/mentionRoutes.js
+++ routes/mentionRoutes.js
@@ -1,9 +1,10 @@
 import express from "express";
-import { addMention, getTrendingStocks } from "../controllers/mentionController.js";
+import { addMention, getTrendingStocks, leastRanking } from "../controllers/mentionController.js";
 
 const router = express.Router();
 
 router.post("/", addMention);         // Add a mention
-router.get("/trending", getTrendingStocks);  // Get trending stocks
+router.get("/top-trending", getTrendingStocks);  // Get trending stocks
+router.get("/bottom-trending", leastRanking);   // Get least trending stocks
 
 export default router;
